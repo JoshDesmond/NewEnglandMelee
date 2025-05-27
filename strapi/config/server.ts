f2731548c2ce0cef18cf8888b3e53f288cd1daf7
Josh Desmond <8055127+JoshDesmond@users.noreply.github.com>
2025-05-27 13:20:42 -0400

    More nginx and strapi config updates

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -1,7 +1,12 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://140.82.5.68'),
   app: {
     keys: env.array('APP_KEYS'),
   },
-});
+  admin: {
+    url: '/admin',
+    serveAdminPanel: true,
+  },
+})
