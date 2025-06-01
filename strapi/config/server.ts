531512ea086b3ef5f788a1f88bce6a233a01b201
Josh Desmond <JoshDesmond@users.noreply.github.com>
2025-05-31 22:39:54 -0400

    Refactor Nginx and Strapi configuration to use subdomains

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -1,12 +1,15 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://140.82.5.68'),
+  url: env('PUBLIC_URL', 'https://api.140.82.5.68'),
   app: {
     keys: env.array('APP_KEYS'),
   },
   admin: {
     url: '/admin',
     serveAdminPanel: true,
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
   },
 })
