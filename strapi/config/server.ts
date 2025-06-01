c301184f1bdc46c64ddf3514f58db408b8437e1a
Josh Desmond <JoshDesmond@users.noreply.github.com>
2025-05-31 22:54:21 -0400

    Adds .xyz domain to configuration

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://api.140.82.5.68'),
+  url: env('PUBLIC_URL', 'https://api.newenglandmelee.xyz'),
   app: {
     keys: env.array('APP_KEYS'),
   },
